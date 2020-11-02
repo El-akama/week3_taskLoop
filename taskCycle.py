74df0ebb6e439ef02c41faf6d0fe5683fc94e63d
El-akama <akmatjanovel@gmail.com>
2020-11-02 15:42:01 +0600

    initial commit

---
+++ taskCycle.py
@@ -0,0 +1,54 @@
+# task1
+# nums = input("enter numbers: ")
+# r = 0
+# for x in nums:
+#     if int(x) > r:
+#         r = int(x)
+# print(r)
+
+# task2
+# import random
+# i = 0
+# num = random.randint(1, 50)
+# print("Загадано число от 1 до 50")
+
+# while True:
+#     you = int(input("угадай число: "))
+#     i += 1
+#     if you < num:
+#         print("это маленькое число")
+#     elif you > num:
+#         print("это большое  число")
+#     elif you == num:
+#         break
+
+# if you == num:
+#     print(f"хорошо, ты угадал за {i} попытки(ок).")
+# elif you != num:
+#     print(f"нет, я загадал {num}.")
+
+# task3 
+# num = int(input("enter number: "))
+# if num > 0:
+#     fact = 1
+# for i in range(2, num + 1):
+#     fact *= i
+# print(fact)
+
+# task4
+# обычная таблица
+# a = range(1, 10)
+# b = a
+# for x in a:
+#     for y in b:
+#         print (f" {x} x {y} = {x*y}")
+#     print(" good")
+
+# таблица пифагора
+# a = range(1, 10)
+# b = a
+# for x in a:
+#     for y in b:
+#         print(f"\t {x*y}", end = '')
+#     print()
+
